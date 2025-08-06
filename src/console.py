369fe0c813b8872057b9da9e49747fa5d1895117
C0lecodes <coledblobban@icloud.com>
2025-08-07 09:45:35 +1200

    console v1 test

--- src/console.py
+++ src/console.py
@@ -12,6 +12,10 @@ buffer = []
 width = 0
 height = 0
 
+def setup(render_input: callable):
+    global render
+    render = render_input
+
 def get_size() -> tuple[int, int]:
     """Return terminal size"""
     try:
@@ -35,33 +39,32 @@ def set(x: int,
     if x >= width or y >= height or x < -width or y < -height: # return if out of bounds
         return
     if len(char) > 1:
-        char = char[0]
+        char = char[0] # if the function gets more than 1 char we use the first char
 
-    buffer[x][y] = char
+    buffer[x][y] = char # sets the cords in the buffer to the char
 
 def write(x: int,
         y: int,
         text: str,
         ):
     """"Set a position in the buffers for strings"""
-
+    # Makes sure the text can fit on screen
     while x < 0:
         x += width
     while y < 0:
         y += height
-
+    # calls the set function on each char
     for i, char in enumerate(str(text)):
         set(x + i, y, char)
 
 def display():
+    """"Display loop"""
 
-    create_buffer()
+    create_buffer() # makes a fresh buffer
+    render() # renders custom frames
 
-    write(0, 0, "TEST")
-
+    # Draws the loaded frames
     for y in range(height):
         for x in range(width):
             print(buffer[x][y], end="", flush=False)
     print("", flush=False)
-
-display()
---
+++ src/ui.py
@@ -0,0 +1,34 @@
+import console
+
+HORIZONTAL_BAR_CHAR = "─"
+VERTICAL_BAR_CHAR = "│"
+PLUS_BAR_CHAR = "┼"
+CORNER_BAR_CHARS = "┌┐└┘"
+T_BAR_CHARS = "├┤┬┴"
+
+COLOUR_GREEN_BOARDERS = (0, 100, 0)
+COLOUR_GREEN_BG = (0, 15, 0)
+COLOUR_BLACK = (0, 0, 0)
+COLOUR_GREEN = 	(102, 255, 102)
+COLOUR_DARK_GREEN = (0, 128, 0)
+COLOUR_WHITE = (192, 192, 192)
+COLOUR_GRAY = (96, 96, 96)
+COLOUR_RED = (255, 0, 0)
+
+
+def render_ui():
+    for x in range(console.width):
+        console.set(x, 0, HORIZONTAL_BAR_CHAR)
+        console.set(x, -1, HORIZONTAL_BAR_CHAR)
+
+    for y in range(console.height):
+        console.set(0, y, VERTICAL_BAR_CHAR)
+        console.set(-1, y, VERTICAL_BAR_CHAR)
+
+    console.set(0,0,CORNER_BAR_CHARS[0])
+    console.set(0,-1,CORNER_BAR_CHARS[2])
+    console.set(-1,0,CORNER_BAR_CHARS[1])
+    console.set(-1,-1,CORNER_BAR_CHARS[3])
+
+console.setup(render_ui)
+console.display()
